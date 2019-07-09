4c552ea104f97be943ce15337f483acc3f2084b9
roshii <roshii@riseup.net>
2019-07-09 14:29:45 +0200

    Update docs

--- src/transaction/Outpoint.jl
+++ src/transaction/Outpoint.jl
@@ -2,11 +2,11 @@
     Outpoint
 
 Because a single transaction can include multiple outputs,
-the Outpoint structure includes both a TXID (or `hash`) and an output `index`
+the Outpoint structure includes both a TXID (or `txid`) and an output `index`
 number to refer to specific output.
-- The TXID of the transaction holding the output to spend.
+- `txid`, the TXID of the transaction holding the output to spend.
   The TXID is a hash provided here in internal byte order.
-- The output index number of the specific output to spend from the transaction.
+- `index`, the output index number of the specific output to spend from the transaction.
   The first output is 0x00000000.
 """
 struct Outpoint
