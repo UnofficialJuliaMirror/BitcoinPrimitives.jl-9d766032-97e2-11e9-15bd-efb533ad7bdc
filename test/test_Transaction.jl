b8f7879cb0d1f7e8b243e90b346cd9f9e0122c2f
roshii <roshii@riseup.net>
2019-07-09 14:22:38 +0200

    Add TxIn tests

--- test/test_Transaction.jl
+++ test/test_Transaction.jl
@@ -3,10 +3,14 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 @testset "Transaction" begin
-    @testset "Outpoint" begin
-        raw = hex2bytes("7b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f00000000")
-        outpoint = Outpoint(IOBuffer(raw))
-        @test bytes2hex(outpoint.txid) == "7b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f"
-        @test outpoint.index == 0
+    @testset "TxIn" begin
+        txin_hex = "7b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f00000000494830450221008949f0cb400094ad2b5eb399d59d01c14d73d8fe6e96df1a7150deb388ab8935022079656090d7f6bac4c9a94e0aad311a4268e082a725f8aeae0573fb12ff866a5f01ffffffff"
+        raw = hex2bytes(txin_hex)
+        txin = TxIn(IOBuffer(raw))
+        @test typeof(txin.previous_output) == Outpoint
+        @test bytes2hex(txin.previous_output.txid) == "7b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f"
+        @test txin.previous_output.index == 0
+        @test txin.signature_script == hex2bytes("4830450221008949f0cb400094ad2b5eb399d59d01c14d73d8fe6e96df1a7150deb388ab8935022079656090d7f6bac4c9a94e0aad311a4268e082a725f8aeae0573fb12ff866a5f01")
+        @test txin.sequence == 0xffffffff
     end
 end
